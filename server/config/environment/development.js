42b68bd5ae49819b6f79bbc3ca798da2b7c31ef7
Adrian Mateoaea <adrianitech@gmail.com>
2015-07-09 12:58:35 +0300

    Removed sensitive info

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -11,9 +11,9 @@ module.exports = {
   seedDB: true,
 
   s3: {
-    folder: 'development-content',
-    bucket: 'beepboop-website',
-    key: "AKIAJH4JBCHEUNK4FBMA",
-    secret: "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"
+    folder: '',
+    bucket: '',
+    key: '',
+    secret: ''
   }
 };
